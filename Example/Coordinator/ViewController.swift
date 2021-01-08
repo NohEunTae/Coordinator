24163466cbd9b5e6afcb1761101dfa80c8dd82c1
NohEunTae <nohet@brandi.co.kr>
2021-01-08 17:41:30 +0900

    Initial commit

---
+++ Example/Coordinator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by NohEunTae on 01/08/2021.
+//  Copyright (c) 2021 NohEunTae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
